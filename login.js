6b51c172a3d92908b918dc25cf0f2b12119574b2
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-18 21:57:45 +0600

    Handle login page

--- login.js
+++ login.js
@@ -1,8 +1,30 @@
+// document
+//   .getElementById("login-btn")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     const phoneNumber = document.getElementById("phone-number");
+//     console.log(phoneNumber.value);
+//     const pinNumber = document.getElementById("pin-number");
+//     console.log(pinNumber.value);
+//     // if (phoneNumber === "5" && pinNumber === "1234") {
+//     //   console.log("You are Log In");
+//     // } else {
+//     //   alert("Wrong phone number or pin");
+//     // }
+//   });
+
 document
   .getElementById("login-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("Login Button Clicked");
-    const phoneNumber = document.getElementById("phone-number");
-    console.log(phoneNumber.value);
+    // Get phone number and pin
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+
+    // validate
+    if (phoneNumber === "5" && pinNumber === "1234") {
+      window.location.href = "/home.html";
+    } else {
+      alert("Wrong Phone Number or Pin");
+    }
   });
