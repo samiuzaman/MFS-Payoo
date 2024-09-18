adb928c7afc73c5fbf3b2bbbb5a8d8f2240e245d
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-18 20:39:57 +0600

    add login number and prevent form

--- login.js
+++ login.js
@@ -1,1 +1,8 @@
-
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("Login Button Clicked");
+    const phoneNumber = document.getElementById("phone-number");
+    console.log(phoneNumber.value);
+  });
