2be37b840f007ed68f9dca418067dcd7cefbe837
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-19 14:56:19 +0600

    Toggle Add money and Cash out Button

--- home/home.js
+++ home/home.js
@@ -1,1 +1,15 @@
+// Open Add money Section
+document
+  .getElementById("add-money-section-btn")
+  .addEventListener("click", function () {
+    document.getElementById("add-money-contaienr").classList.remove("hidden");
+    document.getElementById("cash-out-container").classList.add("hidden");
+  });
 
+// Open Cash Out Section
+document
+  .getElementById("cash-out-section-btn")
+  .addEventListener("click", function () {
+    document.getElementById("cash-out-container").classList.remove("hidden");
+    document.getElementById("add-money-contaienr").classList.add("hidden");
+  });
