095c9c12f4bf1c930fb33858dcf79afd1730fa78
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-19 12:16:15 +0600

    Create add money and cash out option

--- home/home.js
+++ home/home.js
@@ -1,1 +1,18 @@
+// add money to the account
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    // Get money to be added to the account
+    const addMoneyInput = document.getElementById("input-add-money").value;
+    // Get the pin number provided
+    const pinNumber = document.getElementById("input-pin-number").value;
 
+    if (pinNumber === "1234") {
+      const mainBalance = document.getElementById("main-balance").innerText;
+      const newBalance = parseFloat(mainBalance) + parseFloat(addMoneyInput);
+      document.getElementById("main-balance").innerText = newBalance;
+    } else {
+      alert("Faild to add money");
+    }
+  });
